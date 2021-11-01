3dcd655d62ce4d0b8ddf879dbb226ccf510bdeb8
2005935 <dbwls3768pjs@naver.com>
2021-11-02 06:25:11 +0900

    add

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'oitazf=u$2j5g5q2c^y2y^h0@co$bn34w7&e0%jp9_ad*8h4ux'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
